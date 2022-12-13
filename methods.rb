2d02970461fe3c76fddca4526e018290568f35ae
ximenanavarro <ximenanavarro@Ximenas-MacBook-Pro.local>
2022-12-13 11:31:57 -0700

    commit tree 446f0408bd3b83478efdd8afcfebb6eceec20ed0

--- methods.rb
+++ methods.rb
@@ -1,1 +1,29 @@
-# Your code here!
+def greet_programmer 
+    puts "Hello, programmer!"
+end
+
+
+def greet name 
+    puts "Hello, #{name}!"
+end
+
+greet "Naureen"
+
+
+
+def greet_with_default name = "programmer"
+    puts "Hello, #{name}!"
+end
+greet_with_default "Naureen"
+
+
+def add num1, num2
+    return num1 + num2
+end
+
+
+def halve (num)
+    return nil unless num.class == Integer
+
+    num / 2
+end
